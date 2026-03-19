185afa890d79e6c935005d7dbd086fa8213c7aed
ingJuanArenas <juanarenasing@gmail.com>
2026-03-19 16:00:42 -0500

    repositories working successfully

--- src/main/java/com/taller/mecanico/Domain/Repository/OrderRepository.java
+++ src/main/java/com/taller/mecanico/Domain/Repository/OrderRepository.java
@@ -1,24 +1,28 @@
 package com.taller.mecanico.Domain.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+import com.taller.mecanico.Domain.DTOs.CreateOrderDTO;
+import com.taller.mecanico.Domain.DTOs.OrderResponseDTO;
 import com.taller.mecanico.Domain.DTOs.UpdateOrderDTO;
-import com.taller.mecanico.Persistence.Model.OrderStatus;
-import com.taller.mecanico.Persistence.Projections.OrderSummary;
-import com.taller.mecanico.Persistence.Model.Order;
 
 public interface OrderRepository {
-    // interface for getAll, getById, bycustomer, byadvisor, create, update, delete
-    //revisar lo de las fechas
-    List<OrderSummary> getAll();
-    OrderSummary getById(Long id);
-    List<OrderSummary> getByCustomer(Long customerId);
-    List<OrderSummary> getByAdvisor(Long advisorId);
-    List<OrderSummary> getByMechanic(Long mechanicId);
-    List<OrderSummary> getByStatus(OrderStatus status);
-    List<OrderSummary> getByDateRange(LocalDateTime start, LocalDateTime end);
-    OrderSummary create(Order order);
-    OrderSummary update(Long id, UpdateOrderDTO order);
+
+    List<OrderResponseDTO> getAllOrders();
+
+    List<OrderResponseDTO> getAvailableOrders();
+
+    OrderResponseDTO getById(Long id);
+
+    List<OrderResponseDTO> getByCustomer(Long customerId);
+
+    OrderResponseDTO create(CreateOrderDTO order);
+
+    OrderResponseDTO update(Long id, UpdateOrderDTO order);
+
+    OrderResponseDTO takeOrder(Long id, Long mechanicId);
+
+    boolean mechanicHasActiveOrder(Long mechanicId);
+
     void delete(Long id);
 }
--- src/main/java/com/taller/mecanico/Domain/Repository/UserRepositoryInterface.java
+++ src/main/java/com/taller/mecanico/Domain/Repository/UserRepositoryInterface.java
@@ -3,15 +3,20 @@ package com.taller.mecanico.Domain.Repository;
 import java.util.List;
 
 import com.taller.mecanico.Domain.DTOs.UserDTO;
-import com.taller.mecanico.Persistence.Model.UserRole;
-import com.taller.mecanico.Persistence.Projections.UserSummary;
+
 
 public interface UserRepositoryInterface {
+
     List<UserDTO> getAll();
-    List<UserSummary> getByName(String name);
-    List<UserSummary> getByRole(UserRole role);
+
     UserDTO getById(Long id);
+
     UserDTO create(UserDTO user);
+
     UserDTO update(Long id, UserDTO user);
+
     void delete(Long id);
+
+    UserDTO findByAccessCode(String accessCode);
+
 }
