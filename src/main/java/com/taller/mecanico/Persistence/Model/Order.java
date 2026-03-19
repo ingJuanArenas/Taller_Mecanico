0a5c94ee7a10834b6ef3164c8e670a33411ee315
ingJuanArenas <juanarenasing@gmail.com>
2026-03-19 15:56:29 -0500

    order entity

--- src/main/java/com/taller/mecanico/Persistence/Model/Order.java
+++ src/main/java/com/taller/mecanico/Persistence/Model/Order.java
@@ -3,19 +3,19 @@ package com.taller.mecanico.Persistence.Model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -26,16 +26,16 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; //
+    private Long id;
 
     @Column(name = "customer_id", nullable = false)
-    private Long customer; //
+    private Long customerId; 
    
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "advisor_id", nullable = false)
     private User advisor;
    
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mechanic_id")
     private User mechanic;
 
@@ -57,5 +57,9 @@ public class Order {
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+
+
+    
     
 }
